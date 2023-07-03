6ff1c77145913bee4aa852a59286a27380f209a1
Annika Clewis <71907192+annikaClew@users.noreply.github.com>
2023-07-03 16:01:15 -0700

    Created Profile Menu Activity with XML

---
+++ app/src/main/java/com/example/sellingpoint/ProfileMenuActivity.java
@@ -0,0 +1,14 @@
+package com.example.sellingpoint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_menu);
+    }
+}
