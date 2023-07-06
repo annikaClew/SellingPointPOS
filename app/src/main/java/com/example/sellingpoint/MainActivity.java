b511e7a2e2a1931479d9ad3670e2f5d5febea6f2
Annika Clewis <71907192+annikaClew@users.noreply.github.com>
2023-07-06 10:23:36 -0700

    Added functionality to MainActivity's login and signup button to get to respective activities. Also added back button for those activities to go to MainActivity.

--- app/src/main/java/com/example/sellingpoint/MainActivity.java
+++ app/src/main/java/com/example/sellingpoint/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.sellingpoint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginBtn = findViewById(R.id.InitLogIn_btn);
+        Button signupBtn = findViewById(R.id.signup_btn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() { // when user selects login from menu
+            @Override
+            public void onClick(View view) { // go to login activity
+                Intent login = new Intent(view.getContext(), LogInActivity.class);
+                startActivity(login);
+                finish();
+            }
+        });
+
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signup = new Intent(view.getContext(), SignUpActivity.class);
+                startActivity(signup);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/sellingpoint/SignUpActivity.java
+++ app/src/main/java/com/example/sellingpoint/SignUpActivity.java
@@ -5,18 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
-public class LogInActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);
+        setContentView(R.layout.activity_sign_up);
 
-        ImageButton goBack = findViewById(R.id.imageButtonBack);
-        Button login = findViewById(R.id.LogIn_btn);
+        ImageButton goBack = findViewById(R.id.imageButtonBack2);
 
         goBack.setOnClickListener(new View.OnClickListener() { // when user selects back arrow
             @Override
@@ -26,7 +24,5 @@ public class LogInActivity extends AppCompatActivity {
                 finish();
             }
         });
-
-
     }
 }
